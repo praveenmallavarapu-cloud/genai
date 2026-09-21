758ba7bc62b107b4cde7cea908b7d406041ed1cf
praveenmallavarapu-cloud <praveen.mallavarapuai@gmail.com>
2026-09-21 21:42:07 +0530

    pythontest

---
+++ pytest.py
@@ -0,0 +1,20 @@
+print("hello world!") #hello world
+#first comment
+'''line comment
+second line comment'''
+#addition   
+print(2 + 5)        
+#subtraction
+print(5 - 2) #subtraction
+#multiplication
+print(2 * 5) #multiplication
+#division
+print(5 / 2) #division  
+
+x = 90
+y = 10
+
+print("sum of x and y is:", x + y) #sum of x and y  
+print("difference of x and y is:", x - y) #difference of x and y
+print("product of x and y is:", x * y) #product of x and y  
+print("division of x and y is:", x / y) #division of x and y
