f9a1f1348fc62d3fd1d8ff38103b1511a4949c34
praveenmallavarapu-cloud <praveen.mallavarapuai@gmail.com>
2026-09-23 15:17:57 +0530

    dded for loop

--- pythonflowcontrol.py
+++ pythonflowcontrol.py
@@ -1,3 +1,4 @@
+'''
 #if and indentation in python
 
 age = int(input("Enter your age: ")) # taking input from user and converting it to integer
@@ -36,5 +37,27 @@ elif user_input in lovers[1] == "praveen":
     print(" marry") # printing message if condition is false 
 else:
     print("not marry") # printing message if condition is false   
+# greatest number     
+n1 = int(input("Enter first number: ")) # taking input from user and converting it to integer
+n2 = int(input("Enter second number: ")) # taking input from user and converting it to integer
+n3 = int(input("Enter third number: ")) # taking input from user and converting it to integer
+
+if n1 >= n2 and n1 >= n3: # checking if n1 is greater than or equal to n2 and n3
+    print("The largest number is:", n1) # printing the largest number   
+elif n2 >= n1 and n2 >= n3: # checking if n2 is greater than or equal to n1 and n3
+    print("The largest number is:", n2) # printing the largest number   
+else: # if both conditions are false
+    print("The largest number is:", n3) # printing the largest number 
+'''    
+#for loop in python
+
+models = ["BMW", "AUDI", "MERCEDES", "TOYOTA", "HONDA"] # list of car models
+for model in models: # iterating through the list
+    print(model) # printing each model in the list
+    print("\n")
+
+
+
+
+
     
-     
